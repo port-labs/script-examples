3ce3ff80c2de4b19de021051e818327c649d958b
Yair Siman Tov <63305203+yairsimantov20@users.noreply.github.com>
2023-06-29 17:59:09 +0300

    Update delete_entities.py

--- delete_entities.py
+++ delete_entities.py
@@ -1,68 +1,76 @@
 import json
 import requests
+import threading
 ​
-API_URL = 'https://api.getport.io/v1'​
+API_URL = "https://api.getport.io/v1"
+MAX_THREADS = 5
+sema = threading.Semaphore(value=MAX_THREADS)
 ​
-CLIENT_ID = 'CLIENT_ID'
-CLIENT_SECRET = 'CLIENT_SECRET'
 ​
-API_URL = 'https://app.getport.io'
+CLIENT_ID = "CLIENT_ID"
+CLIENT_SECRET = "CLIENT_SECRET"
 ​
-credentials = {'clientId': CLIENT_ID, 'clientSecret': CLIENT_SECRET}
+API_URL = "https://api.getport.io/v1"
 ​
-token_response = requests.post(f'{API_URL}/auth/access_token', json=credentials)
+credentials = {"clientId": CLIENT_ID, "clientSecret": CLIENT_SECRET}
 ​
-access_token = token_response.json()['accessToken']
+token_response = requests.post(f"{API_URL}/auth/access_token", json=credentials)
 ​
-headers = {
-    'Authorization': f'Bearer {access_token}'
-}
+access_token = token_response.json()["accessToken"]
+​
+print(access_token)
+​
+headers = {"Authorization": f"Bearer {access_token}"}
 ​
 ​
 def delete_port_entity(blueprint, entity):
-    res = requests.delete(f"{API_URL}/blueprints/{blueprint}/entities/{entity}?delete_dependents=true", headers=headers)
+    sema.acquire()
+    res = requests.delete(
+        f"{API_URL}/blueprints/{blueprint}/entities/{entity}?delete_dependents=true",
+        headers=headers,
+    )
+    sema.release()
     if res.status_code == 200:
-        print(f'Entity {entity} deleted')
+        print(f"Entity {entity} deleted")
     else:
-        print(f'Encountered error deleting {entity}: {res.json()}')
+        print(f"Encountered error deleting {entity}: {res.json()}")
 ​
 ​
 def delete_all(blueprints):
+    report_threads = []
     for blue in blueprints:
         search = {
             "combinator": "and",
-            "rules": [
-                {
-                    "property": "$blueprint",
-                    "value": blue,
-                    "operator": "="
-                }
-            ]
+            "rules": [{"property": "$blueprint", "value": blue, "operator": "="}],
         }
         entities = requests.post(f"{API_URL}/entities/search", headers=headers, json=search)
-        print(f'Starting to delete all {blue} entities')
+        print(f"Starting to delete all {blue} entities")
         res = entities.json()
-        for ent in res['entities']:
-            delete_port_entity(blue, ent['identifier'])
+        for ent in res["entities"]:
+            report_thread = threading.Thread(target=delete_port_entity, args=(blue, ent["identifier"]))
+            report_thread.start()
+            report_threads.append(report_thread)
+​
+        for thread in report_threads:
+            thread.join()
 ​​
 ​
 all_blueprints = requests.get(f"{API_URL}/blueprints", headers=headers)
 ​
 if all_blueprints.status_code != 200:
-    print('Failed to get blueprints, exiting')
+    print("Failed to get blueprints, exiting")
     exit(1)
 ​
 blueprints = []
 ​
-for blueprint in all_blueprints.json()['blueprints']:
-    blueprints.append(blueprint['identifier'])
-    print('identifier:', blueprint['identifier'], 'title:', blueprint['title'])
+for blueprint in all_blueprints.json()["blueprints"]:
+    blueprints.append(blueprint["identifier"])
 ​
 # Enter here the identifiers of the blueprints you want to delete all entities from
 # blueprints = ['pod', 'workload', 'workflow-run', 'workflow', 'pullRequest', 'node', 'namespace', 'cluster', 'issue', 'microservice']
 ​
 delete_all(blueprints)
 
+
 ​
-​
-print('Done deleting')
+print("Done deleting")
