5f7224e73c3141604561b70cfcc045bd1bcd20fc
OmriGez <omri@getport.io>
2023-11-12 14:16:16 +0200

    final

--- migrateBlueprints.sh
+++ migrateBlueprints.sh
@@ -7,17 +7,17 @@ curl -X 'GET' "https://api.getport.io/v1/blueprints" -H "Authorization: $TOKEN" 
 
 
 # Set Bearer value for new organization
-export TOKEN="xxxx"
+export NEW_TOKEN="xxxx"
 # Create blueprints without relations and mirrorProperties
 cat blueprints.json | jq -c '.blueprints[]' \
   | while read i; do; \
     jq --null-input --argjson i $i -c '$i | del(.relations, .mirrorProperties)' \
-    | curl -s -X POST "https://api.getport.io/v1/blueprints" -H 'Content-Type: application/json' -H "Authorization: $TOKEN" --data-binary @- | jq \
+    | curl -s -X POST "https://api.getport.io/v1/blueprints" -H 'Content-Type: application/json' -H "Authorization: $NEW_TOKEN" --data-binary @- | jq \
   ; done
 # Replace the blueprints including the relations and mirrorProperties
 cat blueprints.json | jq -c '.blueprints[]' \
   | while read i; do; \
     export ID="$(jq -r --null-input --argjson i $i '$i.identifier')" \
     && jq --null-input --argjson i $i -c '$i' \
-    | curl -s -X PUT "https://api.getport.io/v1/blueprints/$ID" -H 'Content-Type: application/json' -H "Authorization: $TOKEN" --data-binary @- | jq \
+    | curl -s -X PUT "https://api.getport.io/v1/blueprints/$ID" -H 'Content-Type: application/json' -H "Authorization: $NEW_TOKEN" --data-binary @- | jq \
   ; done
