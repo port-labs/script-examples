48583832f6c7104382dbc4b39bded012341ca301
OmriGez <omri@getport.io>
2023-11-12 14:09:38 +0200

    final

--- migrateBlueprints.sh
+++ migrateBlueprints.sh
@@ -1,18 +1,12 @@
 # Migrate your Blueprints to a different account
 
-The purpose of this file is to show you how to migrate Blueprints between Port Orgainzations, using a shell script.
-
-
-First, export all of the blueprints using the following script (Make sure to use the API token of the old organization
-# Set Bearer value
+# Set Bearer value for old organization
 export TOKEN="xxxx"
 # Get existing blueprints and save them to blueprints.json
 curl -X 'GET' "https://api.getport.io/v1/blueprints" -H "Authorization: $TOKEN" | jq > blueprints.json
-```
 
-Then, Import all of the blueprints using this script (Make sure to use the API token of the new organization)
-```
-# Set Bearer value
+
+# Set Bearer value for new organization
 export TOKEN="xxxx"
 # Create blueprints without relations and mirrorProperties
 cat blueprints.json | jq -c '.blueprints[]' \
@@ -27,4 +21,3 @@ cat blueprints.json | jq -c '.blueprints[]' \
     && jq --null-input --argjson i $i -c '$i' \
     | curl -s -X PUT "https://api.getport.io/v1/blueprints/$ID" -H 'Content-Type: application/json' -H "Authorization: $TOKEN" --data-binary @- | jq \
   ; done
-```
